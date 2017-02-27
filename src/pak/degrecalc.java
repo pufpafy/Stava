0b257d32a91a6aace0ae59e99268235a8b6ee6da
George <George@Gosho>
2017-02-27 18:11:19 +0200

    с три стави

--- src/pak/degrecalc.java
+++ src/pak/degrecalc.java
@@ -12,7 +12,7 @@ package pak;
 public class degrecalc {
     private int pasd_deg, pasd_lngt2, cordX=0, cordY=0;
     private final int max=255, min=0, deg_max=180, deg_min=0;
-    private double degre, degconst;
+    private double degre;
     public degrecalc () {} // конструктор без пар. препоръчително е да не се използва
     public degrecalc (int form_deg, int form_lngt2) throws Exception {
         //конст. с пар.
@@ -22,18 +22,13 @@ public class degrecalc {
         todegree();
         cord();
     }
-    public void todegree () throws Exception{
-        if (pasd_deg > max || pasd_deg < min) {
-            //проверка на входа
-            throw new outofboudsException("числото не е в позволената граница");
-        }
+    public void todegree (){
+        double degconst;
         //операции
         degconst = (double)deg_max / (double)max;
         degre = (double)pasd_deg * degconst;
-        //
-        if ( (int)degre < deg_min || (int)degre > deg_max) {
-            //проверка на изхода
-            throw new outofboudsException("възникна грешка при смятане на градуса");
+        if (degre > 360) {
+            //degre = degre - (double)360;
         }
     }
     public double getDegree () {
@@ -44,26 +39,20 @@ public class degrecalc {
         double x1 , y1;
         double beta,alpha;
         //необходимо е да се използват два различни метода зависимосто от дали е над 90 или под
-        if ( degre <= 90 && degre >= 0) {
+        //if ( degre >= 0 && degre <= 360 ) {
             alpha = (degre*Math.PI) / (double)180; //трябва да се превърнат в радиани за sin ф.
             beta = (double)90 - degre;
             beta = (beta*Math.PI) / (double)180; //трябва да се превърнат в радиани за sin ф.
             x1 = (double)pasd_lngt2 * Math.sin(alpha);
             y1 = (double)pasd_lngt2 * Math.sin(beta);
-        }
-        else if ( degre > 90 && degre <= 180) {
-            alpha = deg_max - degre;
-            beta = 90 - alpha;
-            alpha = (alpha*Math.PI) / (double)180; //трябва да се превърнат в радиани за sin ф.
-            beta = (beta*Math.PI) / (double)180; //трябва да се превърнат в радиани за sin ф.
-            x1 = (double)pasd_lngt2 * Math.sin(alpha);
-            y1 = (double)pasd_lngt2 * Math.sin(beta);
-        }
-        else {
+            /*
+        }else {
             //в случей че ислезне от 0-180 границата
             throw new outofboudsException("възникна грешка при смятане на кординати");
         }   // да се добавявт още методи ако ще се работи с над 180 градуса
+            */
         //задавамеги на полетата
+            
         cordX = (int)x1;
         cordY = (int)y1;
     }
